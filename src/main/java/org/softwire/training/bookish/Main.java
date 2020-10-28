43cc01c375ce64c4740de0d7b3a328c44c2994e6
William Rothery <will.rothery@live.co.uk>
2020-10-28 14:42:10 +0000

    alphabetical search tech

--- src/main/java/org/softwire/training/bookish/Main.java
+++ src/main/java/org/softwire/training/bookish/Main.java
@@ -1,47 +1,42 @@
 package org.softwire.training.bookish;
 
 import org.jdbi.v3.core.Jdbi;
+import org.softwire.training.bookish.models.database.Book;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Properties;
 
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
         String hostname = "localhost";
-        String database = "bookish";
-        String user = "bookish";
-        String password = "bookish";
+        String database = "bookish_schema";
+        String user = "root";
+        String password = "";
         String connectionString = "jdbc:mysql://" + hostname + "/" + database + "?user=" + user + "&password=" + password + "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=GMT&useSSL=false";
 
         jdbcMethod(connectionString);
-        jdbiMethod(connectionString);
     }
 
     private static void jdbcMethod(String connectionString) throws SQLException {
-        System.out.println("JDBC method...");
-
-        // TODO: print out the details of all the books (using JDBC)
-        // See this page for details: https://docs.oracle.com/javase/tutorial/jdbc/basics/processingsqlstatements.html
-
+        ArrayList<String> bookTitle = new ArrayList<String>();
         Connection connection = DriverManager.getConnection(connectionString);
 
+        String titleQuery = "select title from book";
+        try (Statement stmt = connection.createStatement()) {
+            ResultSet rs = stmt.executeQuery(titleQuery);
+            while (rs.next()) {
+                String title = rs.getString("title");
+                bookTitle.add(title);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());;
+        }
 
-
-    }
-
-    private static void jdbiMethod(String connectionString) {
-        System.out.println("\nJDBI method...");
-
-        // TODO: print out the details of all the books (using JDBI)
-        // See this page for details: http://jdbi.org
-        // Use the "Book" class that we've created for you (in the models.database folder)
-
-        Jdbi jdbi = Jdbi.create(connectionString);
-
-
-
+        Collections.sort(bookTitle);
+        System.out.println(bookTitle);
     }
 }
--- src/main/java/org/softwire/training/bookish/models/database/Book.java
+++ src/main/java/org/softwire/training/bookish/models/database/Book.java
@@ -1,5 +1,70 @@
 package org.softwire.training.bookish.models.database;
 
 public class Book {
+    private int id = this.id;
+    private String title = this.title;
+    private String author = this.author;
+    private String genre = this.genre;
+    private int year = this.year;
+    private int no_in_stock = this.no_in_stock;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getNo_in_stock() {
+        return no_in_stock;
+    }
+
+    public void setNo_in_stock(int no_in_stock) {
+        this.no_in_stock = no_in_stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", genre='" + genre + '\'' +
+                ", year=" + year +
+                ", no_in_stock=" + no_in_stock +
+                '}';
+    }
 }
